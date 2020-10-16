0054ab0d0957676f8954a1db9e56ce262e43ce05
Alex Morton <am@alexmortoncreative.com>
2020-10-16 15:26:57 +0200

    Updated list of cities to fit within narrower site container

--- src/components/DisplayCityList.js
+++ src/components/DisplayCityList.js
@@ -5,11 +5,11 @@ import React from 'react';
 export default function DisplayCityList({ onClick, list }) {
   return (
     <div className="city-list-display">
-      <button className="button" onClick={onClick}>
+      <button onClick={onClick}>
         Click to display the list of available cities
       </button>
 
-      <ul className="city-list">
+      <ul className="city-list container">
         {
           list.map(el => (
             <li key={list.indexOf(el)}>{el}</li>
